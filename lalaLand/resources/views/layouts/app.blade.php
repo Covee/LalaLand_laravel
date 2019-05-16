be8dc9a0b60e90da95f82d9b9ffbe1c1210b3e22
Cobee <uniqtop91@gmail.com>
2019-05-16 14:40:45 +0900

    changed main logo | added sqlite

--- lalaLand/resources/views/layouts/app.blade.php
+++ lalaLand/resources/views/layouts/app.blade.php
@@ -23,8 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div class="pr-3"><img src="/svg/mainLogo.svg"></div>
+                    <div>|</div>
+                    <div class="pl-3" style="font-size: 20px;">LalaLand</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
