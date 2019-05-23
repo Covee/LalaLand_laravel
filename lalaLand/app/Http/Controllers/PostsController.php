582e0947b7f17d8ea9a80e62de5119c4419bb979
Cobee <uniqtop91@gmail.com>
2019-05-23 19:34:15 +0900

    created 'add post' function | validation | required auth for adding new post

--- lalaLand/app/Http/Controllers/PostsController.php
+++ lalaLand/app/Http/Controllers/PostsController.php
@@ -6,8 +6,28 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    // This part requires user to be logged in(authenticated)
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'another' => '',
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+
+        auth()->user()->posts()->create($data);
+
+        dd(request()->all());
     }
 }
