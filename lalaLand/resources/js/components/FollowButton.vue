5070f611174dfce051c989410d4b8538e81e7009
Cobee <uniqtop91@gmail.com>
2019-05-31 13:44:54 +0900

    added functional part of 'following'

--- lalaLand/resources/js/components/FollowButton.vue
+++ lalaLand/resources/js/components/FollowButton.vue
@@ -1,23 +1,36 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-3 mb-1" @click="followUser">Follow</button>
+        <button class="btn btn-primary ml-3 mb-1" @click="followUser" v-text="buttonText"></button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['userId'],
+        props: ['userId', 'follows'],
 
         mounted() {
             console.log('Component mounted.')
+        },
+
+        data: function () {
+            return {
+                status: this.follows,
+            }
         },
 
         methods: {
             followUser() {
                 axios.post('/follow/1')
                     .then(response => {
-                        alert(response.data);
+                        this.status = ! this.status;
+                        
                     });
+            }
+        },
+
+        computed: {
+            buttonText() {
+                return (this.status) ? 'Unfollow' : 'Follow';
             }
         }
     }
